f13cf170a4e6e6c13511a331b0493d653d51c0d2
ME-glitch <yuupvengelshoven@gmail.com>
2020-08-08 15:50:39 +0200

    Updated with pycharm and added it to requirements.txt

--- game/controller.py
+++ game/controller.py
@@ -1,6 +1,10 @@
+from .viewModel import ViewModel
+from .model import Model
 
 
 class Controller:
 
     def __init__(self):
+        self.view = ViewModel()
+        self.model = Model()
         pass
--- game/viewModel.py
+++ game/viewModel.py
@@ -1,6 +1,7 @@
-
+from .view import View
 
 class ViewModel:
 
     def __init__(self):
+        self.view = View()
         pass
